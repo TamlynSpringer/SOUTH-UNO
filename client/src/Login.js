4e5740030505d54c1a63843fe1b4a2e32597a9bb
Priscilla Reboucas <priscillabreboucas@gmail.com>
2022-11-23 20:27:04 +0100

    added all cards to the firestore

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,24 +3,14 @@ import { UnoContext } from './UnoContext';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const [response, setResponse] = useState();
-  const {socket, setUsername, username, room, setRoom, userList, setUserList, user, setUser, fetchCards, fireCards, svgCards} = useContext(UnoContext);
+  const {socket, setUsername, user, setUser, fetchCards, fireCards} = useContext(UnoContext);
 
   const navigate = useNavigate();
-  let deck;
   const joinRoom = async (e) => {
-    let initialDeck = []
-    console.log(initialDeck);
-
     e.preventDefault();
     if(e.target[0].value && e.target[1].value) {
-       
-        socket.on('initialDeck', (deck) => {
-          initialDeck.push(deck);
-        })
         setUsername(e.target[0].value);
-        socket.emit('joinRoom', {room : e.target[1].value, user: e.target[0].value, deck: initialDeck})
-        // socket.emit('handleDeck', initialDeck)
+        socket.emit('joinRoom', {room : e.target[1].value, user: e.target[0].value})
         setUser([...user, {user: e.target[0].value, room: e.target[1].value}])
         navigate(`${e.target[1].value}`)
       }
