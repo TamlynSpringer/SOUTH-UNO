624c1cda103614c416583442e0521f6c5b9faa2e
TamlynSpringer <tamlynspringer@gmail.com>
2022-11-27 22:43:03 +0100

    Added a navbar and uno rules

--- client/src/Home.js
+++ client/src/Home.js
@@ -21,20 +21,9 @@ const Home = () => {
   console.log(userDataList, 'userDataList length in home')
 
   return (
-    <>
-      <header className="header">
-        <h1 className="header__h1">SOUTH UNO</h1>
-        <Link to='login' className="header__links">
-          <h2 className="header__links">Join</h2>
-        </Link>
-      </header>
-      <section className="section__logo">
-        <img className="uno-logo" src={uno} alt="uno-logo" />
-      </section>
-      {/* <main>
-        <Svg/>
-      </main> */}
-    </>
+    <section className="section__logo">
+      <img className="uno-logo" src={uno} alt="uno-logo" />
+    </section>
   )
 }
 
