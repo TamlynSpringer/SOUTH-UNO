56fbfc397df2dfde37436d9d726e8cb7b26e0669
Priscilla Reboucas <priscillabreboucas@gmail.com>
2022-11-30 10:26:10 +0100

    added open to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 import { Link } from "react-router-dom";
 
 
- const Navbar = () => {
+ const Navbar = ({open}) => {
 
   return (
         <ul className={ open? 'nav__display' : 'nav__hide'}>
